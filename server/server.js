9cd19298b920bd8a0d519032e29e6ca4e3874344
Yogita Sheth <yogitasheth@Yogitas-MacBook-Pro.local>
2018-05-08 20:08:44 -0700

    installed redis

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-require('newrelic');
+require('../newrelic');
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
@@ -8,6 +8,15 @@ const pgp = require('pg-promise')();
 // const { Client, Pool } = require('pg');
 
 const app = express();
+var redis = require('redis');
+var client = redis.createClient();
+client.on('ready',function() {
+ console.log("Redis is ready");
+});
+
+client.on('error',function() {
+ console.log("Error in Redis");
+});
 const port = process.env.PORT || 3001;
 const connectionObj = {
   user: 'yogitasheth',
@@ -44,26 +53,7 @@ app.get('/restaurants/:id', (req, res) => {
 //   });
 // });
 
-
-// get data from from postgressql
-// app.get('/api/restaurants/:id', function (req, res) {
-//      const query = {
-//        // give the query a unique name
-//        name: 'fetch-user',
-//        text: 'SELECT * FROM apateezside WHERE id = $1',
-//        values: [req.params.id]
-//      }
-//      client.connect();
-//      client.query(query,function(err,result) {
-//           if(err){
-//              res.status(400).send(err);
-//           }else{
-//             res.status(200).send(result.rows[0]);
-//           }
-//      });
-// });
-
-app.get('/api/restaurants/:id', (req, res) => {
+var getRestaurant = (req, res) => {
   const query = {
     // give the query a unique name
     name: 'fetch-user',
@@ -78,7 +68,24 @@ app.get('/api/restaurants/:id', (req, res) => {
     .catch((error) => {
       res.status(400).send(error);
     });
-});
+}
+var getRestaurant = (req, res) => {
+  const query = {
+    // give the query a unique name
+    name: 'fetch-user',
+    text: 'SELECT * FROM apateezside WHERE id = $1',
+    values: [req.params.id],
+  };
+  db.one(query)
+    .then((result) => {
+      console.log(result);
+      res.status(200).send(result);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+};
+app.get('/api/restaurants/:id', getRestaurant);
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
