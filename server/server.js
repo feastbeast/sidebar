35aea68f4eef6e68c18a031cb4b82467fd1d4d57
Yogita Sheth <yogitasheth@Yogitas-MacBook-Pro.local>
2018-05-09 19:39:11 -0700

    testing ways to optimize using http max sockets

--- server/server.js
+++ server/server.js
@@ -1,14 +1,30 @@
 const newrelic = require('newrelic');
+const http = require('http');
 const express = require('express');
+const compression = require('compression');
 const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 const Places = require('../database/index.js');
 const pgp = require('pg-promise')();
 // const { Client, Pool } = require('pg');
-
+http.globalAgent.maxSockets = Infinity;
+http.globalAgent.keepAlive = true;
+http.globalAgent.maxFreeSockets = Infinity;
 const app = express();
+var server = http.createServer(app); 
 const redis = require('redis');
+// var express = require('express');
+// var app = express();
+// var server = app.listen(5001);
+
+// server.listen(3000);
+
+// server.on('connection', function(socket) {
+//   console.log("A new connection was made by a client.");
+//   socket.setTimeout(30 * 1000); 
+//   // 30 second timeout. Change this as you see fit.
+// })
 
 const client = redis.createClient();
 client.on('ready', () => {
@@ -29,6 +45,7 @@ const db = pgp(connectionObj);
 // const pool = new Pool(connectionObj);
 // const client = new Client(connectionObj);
 
+app.use(compression());
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -42,8 +59,9 @@ app.use('/restaurants', express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/restaurants/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  //res.end();
 });
-// MongoDB
+//MongoDB
 // app.get('/api/restaurants/:id', function(req, res) {
 //   let q = Places.findOne({id: req.params.id});
 
@@ -63,10 +81,9 @@ const getRestaurant = (req, res) => {
   };
   db.one(query)
     .then((result) => {
-      console.log(`db ${result}`);
-
       client.setex(id, 3600, JSON.stringify(result));
       res.status(200).send(result);
+      //res.flush();
     })
     .catch((error) => {
       res.status(400).send(error);
@@ -76,15 +93,20 @@ const getCache = (req, res) => {
   const id = req.params.id;
   client.get(id, (err, result) => {
     if (result) {
-      console.log(`cached ${result}`);
+      //console.log(`cached ${result}`);
       res.send(result);
+      //res.flush();
+      //res.end();
     } else {
       getRestaurant(req, res);
     }
   });
 };
 app.get('/api/restaurants/:id', getCache);
-
-app.listen(port, () => {
+server.timeout = 0;
+server.keepAliveTimeout=745456464586;
+server.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
-});
+})
+
+
